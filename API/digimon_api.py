3efc4a17816005e563ede0f55035d46b9aba6510
DonEdgar <github@tigers.8shield.net>
2025-08-17 23:20:45 -0500

    Trying to encode json

--- API/digimon_api.py
+++ API/digimon_api.py
@@ -1,12 +1,4 @@
 import requests
-from urlbuilder import urlbuilder
-from query_builder import query_builder
-
-
-params = query_builder()
-
-URL = urlbuilder(params)
-print(URL)
 
 def call_api(URL):
 
@@ -15,6 +7,4 @@ def call_api(URL):
 
     response = requests.request("GET", URL, headers=headers, data=payload)
 
-    print(response.text)
-
-call_api(URL)
+    return response.text
