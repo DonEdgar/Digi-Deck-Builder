b0fdd3163a775c2283c0a445cd653e93563593cb
Eddie Bello <68202258+DonEdgar@users.noreply.github.com>
2025-08-19 16:01:19 -0500

    Added test_api function

--- API/test_api.py
+++ API/test_api.py
@@ -1,4 +1,5 @@
 import requests
+import urlbuilder
 """
 url = "https://digimoncard.io/api-public/search.php?n=Aldamon&desc=include a Digimon card&color=red&type=digimon&attribute=Variable&card=BT4-016&pack=BT-04: Booster Great Legend&sort=name&sortdirection=desc&series=Digimon Card Game&digitype=Wizard&evocost=3&evocolor=Red"
 
@@ -9,6 +10,16 @@ response = requests.request("GET", url, headers=headers, data=payload)
 
 print(response.text)
 """
-# The above is the example provided by Digimoncard.io
 
-BASEURL = "https://digimoncard.io/api-public/search.php"
+test_params = {"n": "Agumon", "color": "red", "level": "3"}
+
+URL = urlbuilder.urlbuilder(test_params)
+
+def test_api(url):
+     url = url
+     
+     payload = {}
+     
+     headers = {}
+     response = requests.request("GET", url, headers=headers, data=payload)
+     print(response.text)
